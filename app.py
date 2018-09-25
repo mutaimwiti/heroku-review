cd672bac3bd8f5c1d9e7e9308076569597508078
Mutai Mwiti <mutaimwiti40@gmail.com>
2018-09-25 09:43:16 +0300

    Second review

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def root():
     return render_template('index.html')
 
 
+@app.route("/second")
+def second_pr():
+    return render_template('prs/second.html')
+
+
 if __name__ == "__main__":
     app.run()
